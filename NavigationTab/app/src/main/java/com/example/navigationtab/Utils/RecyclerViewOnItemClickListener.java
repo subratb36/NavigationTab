7aac9c78d6330a550b04797f65b40338d2637eeb
subratkumar <subratb36@gmail.com>
2017-05-21 17:29:55 +0530

    first commit

---
+++ NavigationTab/app/src/main/java/com/example/navigationtab/Utils/RecyclerViewOnItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.navigationtab.Utils;
+
+import android.view.View;
+
+/**
+ * Created by subratkumar on 21-05-2017.
+ */
+
+public interface RecyclerViewOnItemClickListener {
+    public void onVideoItemClick(VideoItemObject videoItemObject);
+}
